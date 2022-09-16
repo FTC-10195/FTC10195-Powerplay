5b0d7aba5f0c3eb5bd734cfd79e33260bdc87dfc
ayaamghi <ayaamghi@gmail.com>
2022-09-15 21:03:45 -0400

    Update TestClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
     public static void main(String args[]) {
         System.out.println("Hello");
         int x = 5;
-
+        org.firstinspires.ftc.teamcode.TestClass();
     }
 }

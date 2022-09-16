d8d6703f4a6353be14a0fb7167c9603b51704dbc
Keval Shah <57273556+kevalshah2005@users.noreply.github.com>
2022-09-16 13:57:27 -0400

    fixed weird bug in TestClass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -4,7 +4,6 @@ public class TestClass {
     public static void main(String args[]) {
         System.out.println("Hello");
         int x = 5;
-        org.firstinspires.ftc.teamcode.TestClass();
         int y = 2;
         int totalMints = 500;
         int numberOfBags = totalMints/3; //I used integer division as I wanted to cut off any decimal digits that may have been in the answer, but I did want the number of bags so no modulo

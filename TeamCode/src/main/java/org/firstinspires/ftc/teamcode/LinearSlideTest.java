7ac202312b47084502e1c7e4145d979e0a31da88
ayaamghi <ayaamghi@gmail.com>
2022-10-01 13:34:19 -0400

    Changes motor variable name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
@@ -6,25 +6,25 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class LinearSlideTest extends LinearOpMode {
-    DcMotorEx ls; //defines motor
+    DcMotorEx linearSlide; //defines motor
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ls = hardwareMap.get(DcMotorEx.class, "linearSlide"); //Sets motor
-        ls.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ls.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        ls.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlide = hardwareMap.get(DcMotorEx.class, "linearSlide"); //Sets motor
+        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         while (opModeIsActive()) {                  //Controls movement
             boolean y = gamepad1.right_bumper;
             boolean x = gamepad1.left_bumper;
             if (y) {
-                ls.setPower(1);
+                linearSlide.setPower(1);
             }
             if (x) {
-                ls.setPower(-1);
+                linearSlide.setPower(-1);
             }
 
-            telemetry.addData("Encoder Value", ls.getCurrentPosition());
+            telemetry.addData("Encoder Value", linearSlide.getCurrentPosition());
             telemetry.update();
         }
     }

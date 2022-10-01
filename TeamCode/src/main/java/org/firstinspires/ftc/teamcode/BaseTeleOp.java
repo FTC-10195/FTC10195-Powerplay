f5ea0ea5bad37d1670798d9a27e0606920ef4566
ayaamghi <ayaamghi@gmail.com>
2022-09-30 20:54:58 -0400

    Rety of Commit
    Commit of change name did not work or something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseTeleOp.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class BasicTeleOp extends LinearOpMode {
+public class BaseTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors

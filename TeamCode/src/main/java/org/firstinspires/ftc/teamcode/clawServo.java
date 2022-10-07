3aa8f26171533dde550de4cdb33bf61cc3a78536
ayaamghi <ayaamghi@gmail.com>
2022-10-07 19:43:48 -0400

    Fixed Servo Tester for a One Servo Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawServo.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class clawServo extends LinearOpMode {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawServoTesterClassAyaam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clawServoTesterClassAyaam.java
@@ -2,54 +2,44 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class clawServoTesterClassAyaam extends LinearOpMode {
+
     @Override
     public void runOpMode() throws InterruptedException {
-        Servo clawServoOne = hardwareMap.servo.get("clawOne");
-        Servo clawServoTwo = hardwareMap.servo.get("clawTwo");
-        double clawServoPositionOne = 0;
-        double clawServoPositionTwo = 0;
+
+        Servo clawServo = hardwareMap.servo.get("clawOne");
+
+        double clawServoPosition = 0;
+
         boolean previousRightBumper = gamepad1.right_bumper;
+
         boolean previousLeftBumper = gamepad1.left_bumper;
-        boolean previousX = gamepad1.x;
-        boolean previousB = gamepad1.b;
 
         waitForStart();
+
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
 
-            clawServoOne.setPosition(clawServoPositionOne);
-            clawServoTwo.setPosition(clawServoPositionTwo);
+            clawServo.setPosition(clawServoPosition);
 
 
             if (gamepad1.right_bumper && !previousRightBumper) {
-                clawServoPositionOne += .05;
+                clawServoPosition += .05;
             }
+
             if (gamepad1.left_bumper && !previousLeftBumper) {
-                clawServoPositionOne -= .05;
-            }
-            if (gamepad1.x && !previousX) {
-                clawServoPositionTwo += .05;
-            }
-            if (gamepad1.b && !previousB) {
-                clawServoPositionTwo -= .05;
+                clawServoPosition -= .05;
             }
 
-
-                previousRightBumper = gamepad1.right_bumper;
-                previousLeftBumper = gamepad1.left_bumper;
-                previousX = gamepad1.x;
-                previousB = gamepad1.b;
+            previousRightBumper = gamepad1.right_bumper;
+            previousLeftBumper = gamepad1.left_bumper;
 
 
-                telemetry.addData("Servo Position", clawServoPositionOne);
-                telemetry.addData("Servo Position", clawServoPositionTwo);
-
+                telemetry.addData("Servo Position", clawServoPosition);
                 telemetry.update();
             }
         }

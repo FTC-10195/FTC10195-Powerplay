3cf04b4a5e51a8a4015186e807cfb1dc279c17b3
ayaamghi <ayaamghi@gmail.com>
2022-10-24 06:32:35 -0400

    Better testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp
 public class LinearSlideTest extends LinearOpMode {
     DcMotorEx linearSlide; //defines motor
 
@@ -14,6 +16,8 @@ public class LinearSlideTest extends LinearOpMode {
         linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
         while (opModeIsActive()) {                  //Controls movement
             boolean y = gamepad1.right_bumper;
             boolean x = gamepad1.left_bumper;
@@ -24,6 +28,9 @@ public class LinearSlideTest extends LinearOpMode {
                 linearSlide.setPower(-1);
             }
 
+            if(gamepad1.b) {
+                linearSlide.setMotorDisable();
+            }
             telemetry.addData("Encoder Value", linearSlide.getCurrentPosition());
             telemetry.update();
         }

78719506c7a63b1f669c763f6c9b3544104e4b63
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2022-10-28 16:31:18 -0400

    Update IMUSnapping.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUSnapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUSnapping.java
@@ -1,10 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;

87613d0c84dd330c68cfc2e1eda6d3b4cbd6ae3c
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2022-10-28 19:27:53 -0400

    Update ColorDetector.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorDetector.java
@@ -16,6 +16,18 @@ public class ColorDetector {
     int g = colorSensor.green();
     int b = colorSensor.blue();
 
+    int idealpinkr = 0; //placeholder
+    int idealpinkg = 0; //placeholder
+    int idealpinkb = 0; //placeholder
+
+    int idealgoldr = 0; //placeholder
+    int idealgoldg = 0; //placeholder
+    int idealgoldb = 0; //placeholder
+    
+    int idealbluer = 0; //placeholder
+    int idealblueg = 0; //placeholder
+    int idealblueb = 0; //placeholder
+
     /*Idea here: use Math.abs() to find error values from the received rgb values and the "ideal"
     rgb values for every color to see if the result should be counted as that color. The margin
     should probably be around around 40. May want to see if the previous code actually gets a value

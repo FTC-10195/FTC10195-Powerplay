ae002017b204b46b751b5184cc01bb07ec68b69f
ayaamghi <ayaamghi@gmail.com>
2022-11-11 18:40:30 -0500

    Auto please work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoUseThis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoUseThis.java
@@ -16,7 +16,11 @@ public class AutoUseThis extends LinearOpMode {
     DcMotor motorBackLeft;
     DcMotor motorFrontRight;
     DcMotor motorBackRight;
-
+    int br = 0;
+    int bl = 0;
+    int fr = 0;
+    int fl = 0;
+    ;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,11 +28,11 @@ public class AutoUseThis extends LinearOpMode {
         motorBackLeft = hardwareMap.dcMotor.get("bl"); //reverse this
         motorFrontRight = hardwareMap.dcMotor.get("fr");
         motorBackRight = hardwareMap.dcMotor.get("br"); //reverse
-     //   motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        //   motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         //    motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
         //TODO THIS MOTOR NEEDS TO BE REVERSED ON THE MAIN ROBOT, UNCOMMENT LINE WHEN WORKING ON MAIN ROBOT//
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         DcMotor.ZeroPowerBehavior zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE;
         motorBackRight.setZeroPowerBehavior(zeroPowerBehavior);
@@ -46,52 +50,101 @@ public class AutoUseThis extends LinearOpMode {
         motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        int br = motorBackRight.getCurrentPosition();
+        int bl = motorBackLeft.getCurrentPosition();
+        int fr = motorFrontRight.getCurrentPosition();
+        int fl = motorFrontLeft.getCurrentPosition();
+
         waitForStart();
 
 
-
         if (isStopRequested()) return;
-        while (opModeIsActive()) {
-            int br = motorBackRight.getCurrentPosition();
-            int bl =   motorBackLeft.getCurrentPosition();
-            int fr =  motorFrontRight.getCurrentPosition();
-            int fl =  motorFrontLeft.getCurrentPosition();
+        if (opModeIsActive()) {
+            br = motorBackRight.getCurrentPosition();
+            bl = motorBackLeft.getCurrentPosition();
+            fr = motorFrontRight.getCurrentPosition();
+            fl = motorFrontLeft.getCurrentPosition();
             //TODO scrim is cursed i hate it here
+
             forward(1000);
             telemetry.addData("motorBackRight", br);
             telemetry.addData("motorBackLeft", bl);
             telemetry.addData("motorFrontRight", fr);
             telemetry.addData("motorFrontLeft", fl);
             telemetry.update();
-
-
+            sleep(18000);
+            strafeRight(1000);
+            telemetry.addData("motorBackRight", br);
+            telemetry.addData("motorBackLeft", bl);
+            telemetry.addData("motorFrontRight", fr);
+            telemetry.addData("motorFrontLeft", fl);
+            telemetry.update();
         }
     }
 
 
+    public void forward(int distance) {
 
 
+            motorBackRight.setTargetPosition(distance);
+            motorBackLeft.setTargetPosition(distance);
+            motorFrontRight.setTargetPosition(distance);
+            motorFrontLeft.setTargetPosition(distance);
+
+            motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            motorBackRight.setPower(.1);
+            motorBackLeft.setPower(.1);
+            motorFrontRight.setPower(.1);
+            motorFrontLeft.setPower(.1);
+
+            while(!motorBackLeft.isBusy() && !motorBackRight.isBusy() && !motorFrontLeft.isBusy() && !motorFrontRight.isBusy()){}
+                motorBackRight.setPower(0);
+                motorBackLeft.setPower(0);
+                motorFrontRight.setPower(0);
+                motorFrontLeft.setPower(0);
+    }
 
 
-    public int forward(int distance) {
+    public void strafeRight(int distance) {
 
-        motorBackRight.setTargetPosition(distance);
-        motorBackLeft.setTargetPosition(distance);
-        motorFrontRight.setTargetPosition(distance);
-        motorFrontLeft.setTargetPosition(distance);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        motorBackRight.setPower(.1);
-        motorBackLeft.setPower(.1);
-        motorFrontRight.setPower(.1);
-        motorFrontLeft.setPower(.1);
-        return 1;
+            motorBackRight.setTargetPosition(distance);
+            motorBackLeft.setTargetPosition(-distance);
+            motorFrontRight.setTargetPosition(distance);
+            motorFrontLeft.setTargetPosition(-distance);
+
+            motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            motorBackRight.setPower(1);
+            motorBackLeft.setPower(1);
+            motorFrontRight.setPower(1);
+            motorFrontLeft.setPower(1);
+        while(!motorBackLeft.isBusy() && !motorBackRight.isBusy() && !motorFrontLeft.isBusy() && !motorFrontRight.isBusy()){}
+        motorBackRight.setPower(0);
+        motorBackLeft.setPower(0);
+        motorFrontRight.setPower(0);
+        motorFrontLeft.setPower(0);
+
+
 
     }
+
 
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -3,6 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
+
 public class BasicTeleOp extends LinearOpMode {
    //basic teleop for mecanum
     @Override
@@ -16,6 +17,7 @@ public class BasicTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
 
             mecanumDrive.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
     }
 }
 }

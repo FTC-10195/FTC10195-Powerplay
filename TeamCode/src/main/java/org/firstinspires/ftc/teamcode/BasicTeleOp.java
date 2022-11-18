2c84fd1f133aaa57118dcae8f72a438880392622
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2022-11-18 18:39:11 -0500

    Slight Teleop fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -9,19 +9,19 @@ public class BasicTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap);
-        Claw claw = new Claw(hardwareMap);
-        LinearSlide linearSlide = new LinearSlide(hardwareMap);
+        //Claw claw = new Claw(hardwareMap);
+        //LinearSlide linearSlide = new LinearSlide(hardwareMap);
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            claw.intake(gamepad1.a, gamepad1.x);
-            claw.intake(gamepad2.a, gamepad2.x);
+            //claw.intake(gamepad1.a, gamepad1.x);
+            //claw.intake(gamepad2.a, gamepad2.x);
             mecanumDrive.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-            if(gamepad2.dpad_down) {claw.intake(false, true);}
-            linearSlide.slideMovement(gamepad1.dpad_up, gamepad1.dpad_down, gamepad1.dpad_left, gamepad1.dpad_right);
-            linearSlide.manualMove(gamepad1.left_trigger,gamepad1.right_trigger, gamepad1.b);
+            //if(gamepad2.dpad_down) {claw.intake(false, true);}
+            //linearSlide.slideMovement(gamepad1.dpad_up, gamepad1.dpad_down, gamepad1.dpad_left, gamepad1.dpad_right);
+            //linearSlide.manualMove(gamepad1.left_trigger,gamepad1.right_trigger, gamepad1.b);
 
     }
 }

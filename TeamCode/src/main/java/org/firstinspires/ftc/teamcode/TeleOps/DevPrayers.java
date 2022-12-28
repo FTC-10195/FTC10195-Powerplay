3ebc3e22f3cdf0d96bbb8d4b98512a98ac709890
ayaamghi <ayaamghi@gmail.com>
2022-12-28 15:52:54 -0500

    Update DevPrayers.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/DevPrayers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/DevPrayers.java
@@ -13,15 +13,17 @@ import org.firstinspires.ftc.teamcode.Subsystems.VirtualFourBar;
 public class DevPrayers extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, telemetry);
-        ClawBar intake = new ClawBar(hardwareMap);
+      //  MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, telemetry);
+        //ClawBar intake = new ClawBar(hardwareMap);
+        VirtualFourBar bar = new VirtualFourBar(hardwareMap);
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            mecanumDrive.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-            intake.intake(gamepad1.a, gamepad1.x);
+        //    mecanumDrive.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+         //   intake.intake(gamepad1.a, gamepad1.x);
+            bar.rollerRotate(gamepad1.a, gamepad1.b);
         }
 
     }

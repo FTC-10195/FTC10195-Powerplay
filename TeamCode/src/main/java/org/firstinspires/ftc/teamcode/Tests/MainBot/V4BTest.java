10629003b84a2939b65c089fea64a33362f6fdad
ayaamghi <ayaamghi@gmail.com>
2022-12-28 16:21:46 -0500

    Update V4BTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MainBot/V4BTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MainBot/V4BTest.java
@@ -9,18 +9,50 @@ public class V4BTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        Servo one = hardwareMap.servo.get("s1");
+        double clawServo1Position = 0;
+
+    double clawServo2Position = 0;
+
+    //falling edge detector
+    boolean previousRightBumper = gamepad1.right_bumper;
+
+    boolean previousLeftBumper = gamepad1.left_bumper;
+
+    Servo one = hardwareMap.servo.get("s1");
         Servo two = hardwareMap.servo.get("s2");
 
-        waitForStart();
 
+
+        waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            one.setPosition(.5);
-            two.setPosition(.5);
+
+        one.setPosition(clawServo1Position);
+
+        two.setPosition(clawServo2Position);
+
+        if (gamepad1.right_bumper && !previousRightBumper) {
+            clawServo1Position += .05;
+            clawServo2Position += .05;
 
         }
+
+        if (gamepad1.left_bumper && !previousLeftBumper) {
+            clawServo1Position -= .05;
+            clawServo2Position -= .05;
+        }
+
+
+        //falling edge detector, prevents the input from repeating during the loop
+        previousRightBumper = gamepad1.right_bumper;
+        previousLeftBumper = gamepad1.left_bumper;
+
+        telemetry.addData("Servo One Position", clawServo1Position);
+        telemetry.addData("Servo Two Position", clawServo2Position);
+        telemetry.update();
     }
+}
+
 }
